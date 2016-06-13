9671c30aa7cc8a49910fa4b6e4f66040c164f374
Filip Dolník <fdolnik@volny.cz>
2016-06-13 09:16:50 +0200

    Fix Aruba tests.

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 rm -rf Build
 mkdir Build
-xcodebuild -project 'CuckooGenerator.xcodeproj' -scheme 'CuckooGenerator' -configuration 'Release' CONFIGURATION_BUILD_DIR=Build clean build
+xcodebuild -project 'CuckooGenerator.xcodeproj' -scheme 'CuckooGenerator' -configuration 'Release' CONFIGURATION_BUILD_DIR=$(pwd)/Build clean build
 ln -s ../Tests/SourceFiles Build/SourceFiles
 cd Tests
 cucumber
